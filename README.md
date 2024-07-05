ba8c18a17cb01260175f8e1dfc793f24795ed76b
Jason Wedderburn <jasonwj96@gmail.com>
2024-07-05 13:23:20 -0500

    changes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # kaggle-projects
+
+Requires Kaggle credential files stored in %USER%\.kaggle\kaggle.json
---
+++ common/custom_datasets.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from torch.utils.data import Dataset
+
+
+class CustomDataset(Dataset):
+    def __init__(self, features, target):
+        self.features = features
+        self.target = target
+
+    def __len__(self):
+        return len(self.features)
+
+    def __getitem__(self, idx):
+        return self.features[idx], self.target[idx]
