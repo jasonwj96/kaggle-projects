5af2afe2f9a91f0cddfd916f9ddcd31aa306ab82
Jason Wedderburn <jasonwj96@gmail.com>
2025-01-03 16:17:58 -0500

    Changes

--- common/utils.py
+++ common/utils.py
@@ -1,13 +1,2 @@
 import pandas as pd
 import plotly.express as px
-
-def plot_missing_values(dataframe: pd.DataFrame, missing_percent: float = 0.05):
-    missing_values = dataframe.isnull().sum()
-    missing_values = dataframe.isnull().sum() / (dataframe.isnull().sum() + dataframe.count())
-    missing_values = missing_values[missing_values > missing_percent]
-
-    fig = px.bar(data_frame=missing_values, 
-                title='Percent Missing Values by Column')
-    fig.update_layout(template='plotly_dark')
-
-    fig.show()
