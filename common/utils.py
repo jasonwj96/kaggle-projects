1ecfed8e41d4e1276426806a66d849d0bd0b6d87
Jason Wedderburn <jasonwj96@gmail.com>
2025-02-03 02:36:34 -0500

    Added Windows support

--- common/utils.py
+++ common/utils.py
@@ -1,3 +1,4 @@
+import os
 import tomllib
 from pathlib import Path
 
@@ -8,5 +9,10 @@ import plotly.express as px
 def load_dataset(dataset_name) -> pd.DataFrame:
     with open("pyproject.toml", "rb") as f:
         data = tomllib.load(f)
-        dataset_path = Path(data["project"]["data_repository"], dataset_name, "train.csv")
+        dataset_path = Path(data["project"]["data_repository_linux"], dataset_name, "train.csv")
+
+        if os.name == "nt":
+            dataset_path = Path(data["project"]["data_repository_windows"], dataset_name,
+                                "train.csv")
+
         return pd.read_csv(dataset_path)
